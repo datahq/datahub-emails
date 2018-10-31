9b8a942e33411ad195f59936150d795a756cfb15
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2018-10-31 14:23:27 +0400

    [staus pages][m]: on_incident and subscribe_user methods + tests - fixes #1

--- datahub_emails/api.py
+++ datahub_emails/api.py
@@ -3,7 +3,9 @@ import requests
 import json
 
 from .config import mch_user, mch_secret, mch_list_id
+from .config import statuspage_id, statuspage_api_key
 
+statuspage_headers={'Authorization': 'OAuth %s' % statuspage_api_key}
 
 def on_new_user(user_info):
     errors = []
@@ -26,3 +28,71 @@ def on_new_user(user_info):
         logging.error(errors[0])
         return False
     return True
+
+
+def on_incident(incident, publisher, errors='', status='identified'):
+    if isinstance(errors, list):
+        errors = '\n'.join(errors)
+    component_id = _get_component_id(publisher)
+    logging.error(component_id)
+    if component_id is None:
+        logging.info('Component with name %s Not Found' % publisher)
+        return
+
+    payload = {
+      "incident": {
+        "name": incident,
+        "body": errors,
+        "status": status,
+        "component_ids": [component_id]
+      }
+    }
+    resp = requests.post(
+        'https://api.statuspage.io/v1/pages/{page_id}/incidents'.format(
+                                                    page_id=statuspage_id),
+        headers=statuspage_headers,
+        data=json.dumps(payload)
+    )
+    if resp.status_code > 205:
+        logging.info('Not able to connect to https://api.statuspage.io')
+        return
+    return resp.json()
+
+
+def subscribe_user(username, email):
+    component_id = _get_component_id(username)
+    if component_id is None:
+        logging.info('Component with name %s Not Found' % username)
+        return
+
+    payload = json.dumps({
+      "email": email,
+      "component_ids": [
+        component_id
+      ]
+    })
+    resp = requests.post(
+        'https://api.statuspage.io/v1/pages/{page_id}/subscribers'.format(page_id=statuspage_id),
+        headers=statuspage_headers,
+        data=payload
+    )
+    if resp.status_code > 205:
+        logging.info('Not able to connect to https://api.statuspage.io')
+        return
+    return resp.json()
+
+
+def _get_component_id(component_name):
+    resp = requests.get(
+        'https://api.statuspage.io/v1/pages/{page_id}/components'.format(page_id=statuspage_id),
+        headers=statuspage_headers
+    )
+    if resp.status_code > 205:
+        logging.info('Not able to connect to https://api.statuspage.io')
+        return
+    components = resp.json()
+    component_id = None
+    for component in components:
+        if component.get('name') == component_name:
+            component_id = component.get('id')
+    return component_id
